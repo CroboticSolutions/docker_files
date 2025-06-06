07c1ee4f76fec10aae84fb37c6d9144bced98282
Filip Zoric <fzoric8@gmail.com>
2025-06-06 13:31:47 +0200

    [humble-gpu] Init Dockerfile for humble with gpu support

---
+++ ros2/humble_gpu/arm_api2/pull_and_run_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CONTAINER_NAME=arm_api2_cont_one_click_test
+DOCKERHUB_SRC=croboticsolutions
+IMAGE_NAME=arm_api2:humble
+
+# Hook to the current SSH_AUTH_LOCK - since it changes
+# https://www.talkingquickly.co.uk/2021/01/tmux-ssh-agent-forwarding-vs-code/
+ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+
+docker pull $DOCKERHUB_SRC/$IMAGE_NAME
+
+docker run \
+  -it \
+  --network host \
+  --privileged \
+  --volume /dev:/dev \
+  --volume /tmp/.x11-unix:/tmp/.x11-unix \
+  --volume ~/.ssh/ssh_auth_sock:/ssh-agent \
+  --env SSH_AUTH_SOCK=/ssh-agent \
+  --env DISPLAY=$DISPLAY \
+  --env TERM=xterm-256color \
+  --name $CONTAINER_NAME \
+  $DOCKERHUB_SRC/$IMAGE_NAME \
+  /bin/bash

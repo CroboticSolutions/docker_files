db3dec923141cf67ddc05db42ac953e4436c8e6b
zozan <fzoric8@gmail.com>
2025-11-24 16:59:28 +0100

    * Added `composers` i.e. folder that will contain compose file for docker images * Added `ros` for the ROS1 SLAM (FastLIVO) build

--- genom/gz_classic/scripts/setup_genom_ros_bridge.sh
+++ genom/gz_classic/scripts/setup_genom_ros_bridge.sh
@@ -10,6 +10,11 @@ echo "Cloning genom_ros_bridge..."
 cd /root/catkin_ws/src
 git clone git@github.com:CroboticSolutions/ros_gui_bridge.git
 
+# Clone rospy_message_converter
+echo "Cloning rospy_message_converter..."
+cd /root/catkin_ws/src
+git clone git@github.com:DFKI-NI/rospy_message_converter.git
+
 echo "Building catkin_ws..."
 cd /root/catkin_ws
 catkin_make -DCATKIN_ENABLE_TESTING=OFF -DCMAKE_BUILD_TYPE=Release

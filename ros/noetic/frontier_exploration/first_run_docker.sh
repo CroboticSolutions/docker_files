d2edf5f1528817f8111b4a3e70d8e6ae9f51769d
AnaBatinovic <ana.batinovic.ab@gmail.com>
2026-02-28 13:16:01 +0100

    Add dockefile for frontier exploration, create FAST-LIVO2 folder

---
+++ ros/noetic/frontier_exploration/first_run_docker.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+CONTAINER_NAME=$1
+[ -z "$CONTAINER_NAME" ] && CONTAINER_NAME=frontier_exp
+
+IMAGE_NAME=$2
+[ -z "$IMAGE_NAME" ] && IMAGE_NAME=frontier_exp:focal
+
+# If not working, first do: sudo rm -rf /tmp/.docker.xauth
+# It still not working, try running the script as root.
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+
+echo "Preparing Xauthority data..."
+xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
+if [ ! -f $XAUTH ]; then
+    if [ ! -z "$xauth_list" ]; then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+echo "Done."
+echo ""
+echo "Verifying file contents:"
+file $XAUTH
+echo "--> It should say \"X11 Xauthority data\"."
+echo ""
+echo "Permissions:"
+ls -FAlh $XAUTH
+echo ""
+echo "Running docker..."
+
+# Hook to the current SSH_AUTH_LOCK - since it changes
+# https://www.talkingquickly.co.uk/2021/01/tmux-ssh-agent-forwarding-vs-code/
+ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+
+docker run -it \
+  --volume $XSOCK:$XSOCK:rw \
+  --volume $XAUTH:$XAUTH:rw \
+  --env XAUTHORITY=${XAUTH} \
+  --env DISPLAY=$DISPLAY \
+  --env QT_X11_NO_MITSHM=1 \
+  --env="TERM=xterm-256color" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --volume="/dev:/dev" \
+  --volume="/var/run/dbus/:/var/run/dbus/:z" \
+  --volume ~/.ssh/ssh_auth_sock:/ssh-agent \
+  --env SSH_AUTH_SOCK=/ssh-agent \
+  --net=host \
+  --privileged \
+  --gpus all \
+  --name $CONTAINER_NAME \
+  $IMAGE_NAME
